5fb0badde31b224a684b6d76813717c931b101a5
Daniel Somoza Paletta <dasomoza@gmail.com>
2021-12-06 21:10:29 +0100

    Fixed base pathname

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,2 +1,4 @@
-export const DEPLOY_CONTRACT_PATH = "deploy-contract";
-export const HOME_PATH = "/";
+const BASE_PATHNAME = process.env.PUBLIC_URL;
+
+export const DEPLOY_CONTRACT_PATH = `${BASE_PATHNAME}deploy-contract`;
+export const HOME_PATH = `${BASE_PATHNAME}/`;
