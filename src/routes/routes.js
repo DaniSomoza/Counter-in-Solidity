24e099c834248cdaa44de89b773df5a084b178c3
Daniel Somoza Paletta <dasomoza@gmail.com>
2021-12-06 21:15:09 +0100

    fixed pathname in deployment page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
 const BASE_PATHNAME = process.env.PUBLIC_URL;
 
-export const DEPLOY_CONTRACT_PATH = `${BASE_PATHNAME}deploy-contract`;
+export const DEPLOY_CONTRACT_PATH = `${BASE_PATHNAME}/deploy-contract`;
 export const HOME_PATH = `${BASE_PATHNAME}/`;
